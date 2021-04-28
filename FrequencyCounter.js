5e962c30edf5c23e773557243b25bac8ecb9cef2
Myra Khan <trueimpulse@yahoo.com>
2021-04-27 21:02:12 -0400

    modified algo1

--- FrequencyCounter.js
+++ FrequencyCounter.js
@@ -6,17 +6,29 @@ same([1,2,1], [4,4,1]) // false (must be the same frequency)
 */
 
 function same(arr1, arr2){
-    if(arr1.lenght !== arr2.length){
+    if(arr1.length !== arr2.length){
         return false;
     }
     let frequencyCounter1 = {};
     let frequencyCounter2 = {};
-    
+
 
     for(let val of arr1){
         frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
     }
-    for(let val of frequencyCounter2){
+    for(let val of arr2){
         frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
     }
+
+    for(let key in frequencyCounter1){
+        if(!(key ** 2 in frequencyCounter2)){
+            return false
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            return false;
+        }
+        return true;
+    }
 }
+
+same([1,2,3,2], [9,1,4,4]);
